67f4d88d5413688b77b655985dc70ae1cd4d8059
brandonstevenz2002 <brandonstevenvm@gmail.com>
2025-08-20 15:58:57 -0500

    Subiendo Myself

---
+++ src/main/kotlin/edu/unicatolica/kotlin/MySelfCristhyan.kt
@@ -0,0 +1,11 @@
+package edu.unicatolica.kotlin
+
+fun main (){
+    val name: String = "Cristhyan Camilo Vera"
+    val age: Int = 24
+    val program: String = "Tec.Desarrollo de software"
+    val interest: String = "Tengo como meta ser master en python"
+
+    val message = "Hola mi nombre es $name, tengo $age años  estoy en el programa $program, mi interes es $interest"
+    println(message)
+}
--- src/main/kotlin/edu/unicatolica/kotlin/VariablesYTipos.kt
+++ src/main/kotlin/edu/unicatolica/kotlin/VariablesYTipos.kt
@@ -1,6 +1,31 @@
 // Variables&Types.kt
 package edu.unicatolica.kotlin
 
+
 fun main() {
     // variables and data types
+    println("Hola mundo")
+
+    //val name1 = "Cristhyan Camilo"
+    //val name2 = "Vera"
+
+//    val number1: Int = 1
+    //  val number2: Long = 3L
+    //val number3: Float = 3.14f
+    //val number4: Double = 2.4
+
+    //val adding = 10 + 1
+    //val subtraction = 10 - 1
+    //val product = 10 * 1
+    //val division =10 / 1
+    //val modulating =10 % 1
+
+    val name3 = "Lina"
+    val age = 19
+    val city = "Palmira"
+
+    val message = "Hola, $name3. Your age is $age, and you live in $city"
+    println(message)
+
+
 }
